add8f2a166ce077e22797b3dea666d23dff93ce1
Sunil Jamkatel <sunil.jamkatel@gmail.com>
2019-01-14 17:27:59 -0600

    Nuke and start again

--- demo/custom.js
+++
@@ -1,6 +0,0 @@
-$(".typed").typed({
-	    	strings: ["developer.", "designer.", "artist."],
-	    	typeSpeed: 150,
-	    	backDelay: 1000,
-	    	loop:true
-		});
--- js/custom.js
+++
@@ -1,6 +0,0 @@
-$(".typed").typed({
-	    	strings: ["junior", "hard-working", "smart", "cute"],
-	    	typeSpeed: 100,
-	    	backDelay: 500,
-	    	loop:true 
-		});
